326b2135fd98ebfe741ad73e91c80f8516e06ce6
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-14 05:19:32 +0000

    Automatically backed up by Learn

--- lib/a_working_program.rb
+++ lib/a_working_program.rb
@@ -1,8 +1,4 @@
-puts Hello World! # Correct this to "Hello World!"
+puts "Hello World!"" # Correct this to "Hello World!"
 
 # Your entire program should read:
 # puts "Hello World!"
-
-def a_working_program
-  puts "Hello World!"
-end
